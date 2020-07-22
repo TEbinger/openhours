432ee1cde3adde1de474edd66bc7b5cceb1cf098
TEbinger <thomas.ebinger@gmx.de>
2020-07-22 22:09:09 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ opening_hours = {
 }
 
 minetest.register_chatcommand("openinghours", {
- minetest.chat_send_player("Öffnungszeiten Mo - Fr ".. opening_hours.weekday[1].. "bis".. opening_hours.weekday[2] .." Uhr, Sa./So. ".. opening_hours.weekend[1] .. " bis ".. opening_hours.weekend[2] .. "Uhr.")})
+ minetest.chat_send_player(player:get_player_name(), "Öffnungszeiten Mo - Fr " --.. opening_hours.weekday[1].. "bis".. opening_hours.weekday[2] .." Uhr, Sa./So. ".. opening_hours.weekend[1] .. " bis ".. opening_hours.weekend[2] .. "Uhr.")})
 
 minetest.register_on_joinplayer(function(player)
     if minetest.check_player_privs(player:get_player_name(), {server = true}) then return end
